4745b9d16b322f07de523405f3b145c378852a21
Ken Nakamura <kjn@princeton.edu>
2021-07-06 22:31:34 -0400

    filtering out short trajectories

--- trajectoryFinder.py
+++ trajectoryFinder.py
@@ -52,9 +52,9 @@ talpha = 0.01
 
 
 # waymo data
-MATLAB = spio.loadmat("XH.mat")
-XH = MATLAB['XH']
-XH[0,:] = XH[0,:] + 10
+MATLAB = spio.loadmat("XH2.mat")
+XH = MATLAB['XH2']
+#XH[0,:] = XH[0,:] + 10
 
 # value at each timestep
 data_6d = np.load('new_center_final.npy')
@@ -82,165 +82,169 @@ x_H, y_H, v_H = XH[:,0]
 
 valinterp = scipy.interpolate.RegularGridInterpolator((x1, x2, x3, x4, x5, x6), data_6d[:,:,:,:,:,:,0])
 
-value = 1
-# finding initial state for robot3
-while value > -0.0:
-    x_R = np.random.uniform()*(x0_H2 - x1[0]) + x1[0]
-    y_R= np.random.uniform()*(x3[-1] - x3[0]) + x3[0]
-    v_R = np.random.uniform()*(x5[-1] - x5[0]) + x5[0]
+
+traj = False
+while traj == False:
+    value = 1
+    # finding initial state for robot3
+    while value > -0.0:
+        x_R = np.random.uniform()*(x0_H2 - x1[0]) + x1[0]
+        y_R= np.random.uniform()*(x3[-1] - x3[0]) + x3[0]
+        v_R = np.random.uniform()*(x5[-1] - x5[0]) + x5[0]
     
-    print('random state')
-    print([x_R,x_H, y_R,y_H, v_R, v_R])    
-    # randomly generating a state
-    state = np.array([x_R, x_H, y_R, y_H, v_R, v_H])
-    value = valinterp(state)
-    print(value)
+        print('random state')
+        print([x_R,x_H, y_R,y_H, v_R, v_R])    
+        # randomly generating a state
+        state = np.array([x_R, x_H, y_R, y_H, v_R, v_H])
+        value = valinterp(state)
+        print(value)
 
-    if x_R-x_H > params['overtake']:
-        value = 1
-    elif x_R-x_H < params['lanekeep']:
-        value = 1    
-    elif x_R > x0_H2:
-        value = 1
-    #elif x_R > x_H:
-        #value = 1 
-        #print('ahead h1')
-
-
-x_R_6D = np.zeros(len(tau))
-y_R_6D = np.zeros(len(tau))
-
-x_H1_6D = np.zeros(len(tau))
-y_H1_6D = np.zeros(len(tau))   
-
-x_H2_6D = np.zeros(len(tau))
-y_H2_6D = np.zeros(len(tau)) 
+        if x_R-x_H > params['overtake']:
+            value = 1
+        elif x_R-x_H < params['lanekeep']:
+            value = 1    
+        elif x_R > x0_H2:
+            value = 1
+        #elif x_R > x_H:
+            #value = 1 
+            #print('ahead h1')
 
 
-# calculating states and adding them to trajectory vectors
-steps = 0
-for k in range(len(XH[1,:])):
-    # XH timestep is 0.2 seconds, tau timestep is 0.1 seconds
-    i = 2*k
-    spatDeriv = derivs_6d[:,:,:,:,:,:,:,i]
+    x_R_6D = np.zeros(len(tau))
+    y_R_6D = np.zeros(len(tau))
+
+    x_H1_6D = np.zeros(len(tau))
+    y_H1_6D = np.zeros(len(tau))   
+
+    x_H2_6D = np.zeros(len(tau))
+    y_H2_6D = np.zeros(len(tau)) 
+
+
+    # calculating states and adding them to trajectory vectors
+    steps = 0
+    for k in range(len(XH[1,:])):
+        # XH timestep is 0.2 seconds, tau timestep is 0.1 seconds
+        i = k
+        spatDeriv = derivs_6d[:,:,:,:,:,:,:,i]
     
-    spatDeriv_x3 = spatDeriv[0,:,:,:,:,:,:]
-    spatDeriv_x5 = spatDeriv[1,:,:,:,:,:,:]
+        spatDeriv_x3 = spatDeriv[0,:,:,:,:,:,:]
+        spatDeriv_x5 = spatDeriv[1,:,:,:,:,:,:]
 
     
-    # adding state to vector
-    x_R_6D[k] = x_R
-    x_H1_6D[k] = x_H
-    y_R_6D[k] = y_R
-    y_H1_6D[k] = y_H
+        # adding state to vector
+        x_R_6D[k] = x_R
+        x_H1_6D[k] = x_H
+        y_R_6D[k] = y_R
+        y_H1_6D[k] = y_H
     
-    # how human 2 moves
-    x_H2_6D[k] = x0_H2 + v_H2*(tau[i])
-    y_H2_6D[k] = y0_H2
+        # how human 2 moves
+        x_H2_6D[k] = x0_H2 + v_H2*(tau[i])
+        y_H2_6D[k] = y0_H2
     
     
-    if x_R > params['x1_ul']:
-        print('x_R out of bounds')
+        if x_R > params['x1_ul']:
+            print('x_R out of bounds')
+            print('step')
+            print(k)
+            steps = k
+            break
+        elif x_H > params['x2_ul']:
+            print('x_H out of bounds')
+            print('step')
+            print(k)
+            steps = k
+            break
+        elif v_R > params['x5_ul']:
+            print('v_R out of bounds')
+            print('step')
+            print(k)
+            steps = k
+            break
+        elif v_R < params['x5_ll']:
+            print('v_R out of bounds')
+            print('step')
+            print(k)
+            steps = k
+            break
+        elif v_H > params['x6_ul']:
+            print('v_H out of bounds')
+            print('step')
+            print(k)
+            steps = k
+            break
+        elif v_H < params['x6_ll']:
+            print('v_H out of bounds')
+            print('step')
+            print(k)
+            steps = k
+            break      
+        elif x_R-x_H > params['overtake']:
+            print('overtake')
+            print(k)
+            steps = k
+            break
+        elif x_R-x_H < params['lanekeep']:
+            print('lanekeep')
+            print(k)
+            steps = k
+            break
+
+    
+    
+        print('value')
+        state = np.array([x_R, x_H, y_R, y_H, v_R, v_H])
+        print(valinterp(state))
         print('step')
-        print(k)
-        steps = k
-        break
-    elif x_H > params['x2_ul']:
-        print('x_H out of bounds')
-        print('step')
-        print(k)
-        steps = k
-        break
-    elif v_R > params['x5_ul']:
-        print('v_R out of bounds')
-        print('step')
-        print(k)
-        steps = k
-        break
-    elif v_R < params['x5_ll']:
-        print('v_R out of bounds')
-        print('step')
-        print(k)
-        steps = k
-        break
-    elif v_H > params['x6_ul']:
-        print('v_H out of bounds')
-        print('step')
-        print(k)
-        steps = k
-        break
-    elif v_H < params['x6_ll']:
-        print('v_H out of bounds')
-        print('step')
-        print(k)
-        steps = k
-        break      
-    elif x_R-x_H > params['overtake']:
-        print('overtake')
-        print(k)
-        steps = k
-        break
-    elif x_R-x_H < params['lanekeep']:
-        print('lanekeep')
-        print(k)
-        steps = k
-        break
+        print(k)    
+        p3interp = scipy.interpolate.RegularGridInterpolator((x1, x2, x3, x4, x5, x6), spatDeriv_x3)
+        p5interp = scipy.interpolate.RegularGridInterpolator((x1, x2, x3, x4, x5, x6), spatDeriv_x5)
+
+        deriv_x3 = p3interp(state)
+        deriv_x5 = p5interp(state)
+
+
+        print('deriv3')
+        print(deriv_x3)
+
+        print('deriv5')
+        print(deriv_x5)
 
     
+        # find optimal control/dist
+        if deriv_x5 > 0:
+            accOpt_R = -accMax_R
+        else:
+            accOpt_R = accMax_R
     
-    print('value')
-    state = np.array([x_R, x_H, y_R, y_H, v_R, v_H])
-    print(valinterp(state))
-    print('step')
-    print(k)    
-    p3interp = scipy.interpolate.RegularGridInterpolator((x1, x2, x3, x4, x5, x6), spatDeriv_x3)
-    p5interp = scipy.interpolate.RegularGridInterpolator((x1, x2, x3, x4, x5, x6), spatDeriv_x5)
-
-    deriv_x3 = p3interp(state)
-    deriv_x5 = p5interp(state)
-
-
-    print('deriv3')
-    print(deriv_x3)
-
-    print('deriv5')
-    print(deriv_x5)
-
-    
-    # find optimal control/dist
-    if deriv_x5 > 0:
-        accOpt_R = -accMax_R
-    else:
-        accOpt_R = accMax_R
-    
-    if deriv_x3 > 0:
-        vLatOpt_R = -vLatMax_R
-    else:
-        vLatOpt_R = vLatMax_R
+        if deriv_x3 > 0:
+            vLatOpt_R = -vLatMax_R
+        else:
+            vLatOpt_R = vLatMax_R
    
 
-    '''
-    % Dynamics:
-    %    \dot{x}_1 = x5
-    %    \dot{x}_2 = x6
-    %    \dot{x}_3 = u2
-    %    \dot{x}_4 = d2
-    %    \dot{x}_5 = u1 - talpha * x5
-    %    \dot{x}_6 = d1 - talpha * x6    
-    '''
+        '''
+        % Dynamics:
+        %    \dot{x}_1 = x5
+        %    \dot{x}_2 = x6
+        %    \dot{x}_3 = u2
+        %    \dot{x}_4 = d2
+        %    \dot{x}_5 = u1 - talpha * x5
+        %    \dot{x}_6 = d1 - talpha * x6    
+        '''
 
-    tstep = 0.2
-    x_R = x_R + tstep*v_R
-    y_R = y_R + tstep*vLatOpt_R
-    v_R = v_R + tstep*(accOpt_R - talpha*v_R)
+        tstep = 0.1
+        x_R = x_R + tstep*v_R
+        y_R = y_R + tstep*vLatOpt_R
+        v_R = v_R + tstep*(accOpt_R - talpha*v_R)
     
 
-    x_H = XH[0,k+1]
-    y_H = XH[1,k+1]
-    v_H = XH[2,k+1]
+        x_H = XH[0,k+1]
+        y_H = XH[1,k+1]
+        v_H = XH[2,k+1]
 
-print('found trajectory')
-
+    print('found trajectory')
+    if k >7:
+        traj = True
 robot_traj = np.stack([x_R_6D,y_R_6D], -1)
 H1_traj = np.stack([x_H1_6D, y_H1_6D], -1)
 H2_traj = np.stack([x_H2_6D, y_H2_6D], -1)
